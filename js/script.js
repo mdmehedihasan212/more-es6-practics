d5656eacf13d1dc8d2ec82da69521d03588ac70d
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-14 14:29:04 +0600

    add display dynamic result

--- js/script.js
+++ js/script.js
@@ -3,8 +3,22 @@ fetch("http://universities.hipolabs.com/search?country=United+States")
     .then(data => loadData(data))
 
 const loadData = object => {
+    const displayData = document.getElementById('university-list');
     for (const data of object) {
-        const universities = data.name;
-        console.log(universities);
+        const { country, name, web_pages } = data;
+        // console.log(country, name, web_pages);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card w-50">
+  <div class="card-body">
+    <h3 class="card-title">${name}</h3>
+    <h5 class="card-text">${country}</h5>
+    <a href="${web_pages}" class="btn btn-primary">Web Pages</a>
+  </div>
+</div>
+        `;
+        displayData.appendChild(div);
+
     }
 }
+
